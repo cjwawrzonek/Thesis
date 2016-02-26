a41738ddd4d369458ab1f96ef7caa0b7e706abe4
christian wawrzonek <cjw@nat-tvwna-inside-visitornet101-c-8610.princeton.org>
2016-02-26 09:29:29 -0500

    quick fix

--- saccade/scripts/master.sh
+++ saccade/scripts/master.sh
@@ -4,5 +4,8 @@
 #          Description
 # ------------------------------------------------------------------
 for(( i = 1; i < 37; i++ )); 
+do chmod +x attention$i.sh;
+done
+for(( i = 1; i < 37; i++ )); 
 do sumbit attention$i.sh;
 done
