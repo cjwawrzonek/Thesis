5c7d94b284e3ade98e9e1e016e1f12dc4293e342
christian wawrzonek <cjw@nat-oitwireless-inside-vapornet100-d-16387.Princeton.EDU>
2016-03-04 06:01:30 -0500

    Fixed master.sh for submitting batch runs

--- saccade/scripts/master.sh
+++ saccade/scripts/master.sh
@@ -1,11 +1,59 @@
 #!/bin/bash
 # ------------------------------------------------------------------
-# [Author] Title
-#          Description
+# [CJ Wawrzonek] master.sh
+#          Script for submitting batch experiment runs
 # ------------------------------------------------------------------
-for(( i = 1; i < 37; i++ )); 
-do chmod +x attention$i.sh;
+
+#######################################################
+# attention experiments
+#######################################################
+
+FILE="attention1.sh"
+((i=1))
+while [ -f $FILE ];
+do
+	chmod +x $FILE;
+	submit attention$i.sh;
+	((i++))
+	FILE="attention$i.sh"
 done
-for(( i = 1; i < 37; i++ )); 
-do submit attention$i.sh;
+((i=i-1))
+FILE="attention$i.sh"
+echo "$FILE was the final attention script submitted"
+echo ""
+
+#######################################################
+# selection experiments
+#######################################################
+
+FILE="selection1.sh"
+((i=1))
+while [ -f $FILE ];
+do
+	chmod +x $FILE;
+	submit selection$i.sh;
+	((i++))
+	FILE="selection$i.sh"
 done
+((i=i-1))
+FILE="selection$i.sh"
+echo "$FILE was the final selection script submitted"
+echo ""
+
+#######################################################
+# combined experiments
+#######################################################
+
+FILE="combined1.sh"
+((i=1))
+while [ -f $FILE ];
+do
+	chmod +x $FILE;
+	submit combined$i.sh;
+	((i++))
+	FILE="combined$i.sh"
+done
+((i=i-1))
+FILE="combined$i.sh"
+echo "$FILE was the final combined script submitted"
+echo ""
